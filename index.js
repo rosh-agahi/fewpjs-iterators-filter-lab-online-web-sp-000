02714caa83b05310fdc2d32e3bac0cbc551215c7
Roshanak Agahi <rosh.agahi@gmail.com>
2021-03-20 22:45:54 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solution here
+
+function findMatching() {
+  return list.filter(function (driverName) {
+      return driverName.toLowerCase() === name.toLowerCase();
+    });
+}
+
+function fuzzyMatch() {
+  
+}
+
+function matchName() {
+  
+}
