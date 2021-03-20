918147fc73c42d59442998cbcd45d8a6fea24a1b
Roshanak Agahi <rosh.agahi@gmail.com>
2021-03-20 23:09:13 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,8 +7,11 @@ function findMatching(list, name) {
 }
 
 
-function fuzzyMatch() {
-  
+function fuzzyMatch (list, partialName) {
+  let lengthOfName = partialName.length;
+  return list.filter(function (driverName) {
+    return driverName.slice(0, lengthOfName) === partialName;
+  });
 }
 
 function matchName() {
